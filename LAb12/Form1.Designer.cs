f174184e0278907d62d22c83463e15f2649efe8e
saryunaBadmaeva <90922702+saryunaBadmaeva@users.noreply.github.com>
2022-06-09 00:23:28 +0700

    lab12

---
+++ LAb12/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace LAb12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.btStart = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.edN = new System.Windows.Forms.NumericUpDown();
+            this.edT = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btStart
+            // 
+            this.btStart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btStart.Location = new System.Drawing.Point(37, 390);
+            this.btStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(174, 65);
+            this.btStart.TabIndex = 0;
+            this.btStart.Text = "Start!";
+            this.btStart.UseVisualStyleBackColor = false;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Interval = 1D;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.Maximum = 4D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(248, 18);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1575, 489);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // edN
+            // 
+            this.edN.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edN.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.edN.Location = new System.Drawing.Point(157, 29);
+            this.edN.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.edN.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.edN.Name = "edN";
+            this.edN.Size = new System.Drawing.Size(83, 44);
+            this.edN.TabIndex = 2;
+            this.edN.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // edT
+            // 
+            this.edT.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edT.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.edT.Location = new System.Drawing.Point(126, 103);
+            this.edT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.edT.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.edT.Name = "edT";
+            this.edT.Size = new System.Drawing.Size(114, 44);
+            this.edT.TabIndex = 3;
+            this.edT.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(11, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Amount:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(11, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Time:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart2
+            // 
+            chartArea2.AxisX.Maximum = 4D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(248, 517);
+            this.chart2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(918, 425);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            title1.Name = "Title1";
+            title1.Text = "Stationary probability distribution of weather conditions";
+            this.chart2.Titles.Add(title1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(39, 204);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 37);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "1 - Clear";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(39, 263);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 37);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "2 - Cloudy";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(39, 321);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(190, 37);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "3 - Overcast";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1840, 960);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.edT);
+            this.Controls.Add(this.edN);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown edN;
+        private System.Windows.Forms.NumericUpDown edT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ LAb12/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAb12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double[,] Q = { 
+            { -0.4, 0.3, 0.1 }, 
+            { 0.4, -0.8, 0.4 }, 
+            { 0.1, 0.4, -0.5 } 
+        };
+        double[] probs = new double[3];
+        string[] states = { "Clear", "Cloudy", "Overcast" };
+
+        int T, N, state = 0;
+        double dt, t = 0;
+
+        double[] freq = new double[3];
+
+        int k = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (k < N)
+            {
+                if (t < T)
+                {
+                    dt = Math.Log(rnd.NextDouble()) / Q[state, state];
+                    t += dt;
+                    chart1.Series[0].Points.AddXY(t, state + 1);
+
+                    for (int i = 0; i < states.Length; i++)
+                        if (i != state)
+                            probs[i] = -Q[state, i] / Q[state, state];
+                        else
+                            probs[i] = 0;
+
+                    probability = rnd.NextDouble();
+                    for (int i = 0; i < 3; i++)
+                    {
+                        probability -= probs[i];
+                        if (probability <= 0)
+                        {
+                            state = i;
+                            break;
+                        }
+                    }
+
+                    chart1.Series[0].Points.AddXY(t, state + 1);
+                }
+                else
+                {
+                    freq[state]++;
+                    state = 0;
+                    k++;
+                    t = 0;
+                    chart1.Series[0].Points.Clear();
+                    chart1.Series[0].Points.AddXY(t, state + 1);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    freq[i] = freq[i] / N;
+                    chart2.Series[0].Points.AddXY(i + 1, freq[i]);
+                }
+                timer1.Stop();
+            }
+        }
+
+        Random rnd = new Random();
+        double probability;
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(t, state + 1);
+            chart2.Series[0].Points.Clear();
+            T = (int)edT.Value;
+            N = (int)edN.Value;
+
+          
+            timer1.Start();
+        }
+
+    }
+}
